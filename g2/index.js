4b18d10c84f2c0ccfac76612240e36eba2cbfe69
Rakhimov01 <95426520+Rakhimov01@users.noreply.github.com>
2021-12-20 21:01:37 +0500

    Add files via upload

---
+++ g2/index.js
@@ -0,0 +1,24 @@
+// var s='123456789'
+// if(s.match(/[A-Z]/g)){
+//     console.log(`katta xarf bor`)
+// }else if(s.match(/[a-z]/g)){
+//     console.log('kichik xarf bor')
+// }else{
+//     console.log('xarf yoq')
+// }
+
+
+// var s='webbrain academy accaunt for bag to the ddoor';
+// console.log(s.match(/d/gi).length)
+
+// var s='lorem/ ipsum+ do`lor 1';
+// console.log(s.match(/\w/g))
+// function app(value, value2, value3){
+//     var data='shakhzod shukurullaev';
+//     if(data.includes(value && value2 && value3)){
+//     console.log(true)
+//     }else{
+//         console.log(false)
+//     }
+// }
+// app('otabek', 'elbek', 'shakhzod')
